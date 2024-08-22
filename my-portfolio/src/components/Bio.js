10a8f64e79b596ce18c2136221bc22e2b2b9eb5c
MKavEire <mkavanagh187@gmail.com>
2024-08-22 20:10:19 +0100

    write up bio.js boilerplate

---
+++ my-portfolio/src/components/Bio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/Bio.css'
+
+function Bio() {
+    render (
+        <section id="bio" className="bio">
+            <h2>About Me</h2>
+            <p>Bio goes here ...</p>
+        </section>
+    )
+}
+
+export default Bio;
