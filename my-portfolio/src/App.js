dd597727eee3072ca9112dedd868507dcf0b54cd
MKavEire <mkavanagh187@gmail.com>
2024-08-23 21:44:45 +0100

    refactor app.js code

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -11,9 +11,15 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Bio />
-      <Projects />
-      <Contact />
+      <section id="bio">
+        <Bio />
+      </section>
+      <section id="projects">
+        <Projects />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
       {/* Continue adding other components here ... */}
     </div>
   );
--- my-portfolio/src/components/index.js
+++ my-portfolio/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './Header';
 export { default as Bio } from './Bio';
 export { default as Projects } from './Projects';
+export { default as Contact } from './Contact';
